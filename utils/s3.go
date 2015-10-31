bbbcedf5c9ade4faca3a56fe02ec3c9737750199
techjanitor <puntme@gmail.com>
2015-10-30 22:05:12 -0500

    add amazon s3

--- utils/s3.go
+++ utils/s3.go
@@ -21,7 +21,7 @@ func getS3() (service *session.Session, err error) {
 	// create our session
 	svc := session.New(&aws.Config{
 		Region:      aws.String(config.Settings.Amazon.Region),
-		Credentials: aws.String(creds),
+		Credentials: creds,
 	})
 
 	return
@@ -31,7 +31,7 @@ func getS3() (service *session.Session, err error) {
 // Upload a file to S3
 func UploadS3(filepath, filename string) (err error) {
 
-	svc, err := getS3()
+	session, err := getS3()
 	if err != nil {
 		return
 	}
@@ -42,13 +42,13 @@ func UploadS3(filepath, filename string) (err error) {
 	}
 	defer file.Close()
 
-	uploader := s3manager.NewUploader(svc)
+	uploader := s3manager.NewUploader(session)
 
 	params := &s3manager.UploadInput{
 		Bucket:               aws.String(config.Settings.Amazon.Bucket),
 		Key:                  aws.String(filename),
 		Body:                 file,
-		ServerSideEncryption: s3.ServerSideEncryptionAes256,
+		ServerSideEncryption: aws.String(s3.ServerSideEncryptionAes256),
 	}
 
 	resp, err := uploader.Upload(params)
@@ -60,14 +60,16 @@ func UploadS3(filepath, filename string) (err error) {
 // Delete a file from S3
 func DeleteS3(object string) (err error) {
 
-	svc, err := getS3()
+	session, err := getS3()
 	if err != nil {
 		return
 	}
 
+	svc := s3.New(session)
+
 	params := &s3.DeleteObjectInput{
-		Bucket: config.Settings.Amazon.Bucket,
-		Key:    object,
+		Bucket: aws.String(config.Settings.Amazon.Bucket),
+		Key:    aws.String(object),
 	}
 
 	resp, err := svc.DeleteObject(params)
