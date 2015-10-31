870e803a9a981fdce3b47b8bbbb8bbfc74012e5a
techjanitor <puntme@gmail.com>
2015-10-30 23:00:25 -0500

    add mime type to s3 upload

--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import (
 	"github.com/facebookgo/grace/gracehttp"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
-	"os/signal"
 	"runtime"
-	"syscall"
 
 	"github.com/techjanitor/pram-post/config"
 	c "github.com/techjanitor/pram-post/controllers"
