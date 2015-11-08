69c595a7d72d16269cd83e515fe2c9820fd49171
techjanitor <puntme@gmail.com>
2015-11-08 17:14:03 -0600

    add maxbytesreader

--- controllers/reply.go
+++ controllers/reply.go
@@ -28,14 +28,14 @@ func ReplyController(c *gin.Context) {
 	userdata := c.MustGet("userdata").(u.User)
 
 	// check size of content
-	if r.ContentLength > config.Settings.Limits.ImageMaxSize {
+	if req.ContentLength > config.Settings.Limits.ImageMaxSize {
 		c.JSON(http.StatusExpectationFailed, gin.H{"error_message": e.ErrImageSize.Error()})
 		c.Error(e.ErrImageSize)
 		return
 	}
 
 	// set max bytes reader
-	req.Body = http.MaxBytesReader(c.Writer, req.Body, config.Settings.Limits.ImageMaxSize)
+	req.Body = http.MaxBytesReader(c.Writer, req.Body, int64(config.Settings.Limits.ImageMaxSize))
 
 	err = c.Bind(&rf)
 	if err != nil {
