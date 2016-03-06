af05de0189a7d08c177b12369778bfcc32fdd657
techjanitor <puntme@gmail.com>
2016-03-06 16:32:26 -0600

    add register model test

--- models/register_test.go
+++ models/register_test.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	//"errors"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"testing"
@@ -9,6 +9,23 @@ import (
 	"github.com/eirka/eirka-libs/db"
 	e "github.com/eirka/eirka-libs/errors"
 )
+
+func TestRegisterValidatePasswordEmpty(t *testing.T) {
+
+	var err error
+
+	register := RegisterModel{
+		Name:     "test",
+		Email:    "fart@blah.com",
+		Password: "",
+	}
+
+	err = register.Validate()
+	if assert.Error(t, err, "An error was expected") {
+		assert.Equal(t, err, e.ErrPasswordEmpty, "Error should match")
+	}
+
+}
 
 func TestRegisterValidatePasswordShort(t *testing.T) {
 
@@ -24,6 +41,72 @@ func TestRegisterValidatePasswordShort(t *testing.T) {
 	if assert.Error(t, err, "An error was expected") {
 		assert.Equal(t, err, e.ErrPasswordShort, "Error should match")
 	}
+
+}
+
+func TestRegisterValidateNameEmpty(t *testing.T) {
+
+	var err error
+
+	register := RegisterModel{
+		Name:     "",
+		Email:    "fart@blah.com",
+		Password: "password",
+	}
+
+	err = register.Validate()
+	if assert.Error(t, err, "An error was expected") {
+		assert.Equal(t, err, e.ErrNameEmpty, "Error should match")
+	}
+
+}
+
+func TestRegisterValidateNameShort(t *testing.T) {
+
+	var err error
+
+	register := RegisterModel{
+		Name:     "te",
+		Email:    "fart@blah.com",
+		Password: "password",
+	}
+
+	err = register.Validate()
+	if assert.Error(t, err, "An error was expected") {
+		assert.Equal(t, err, e.ErrPasswordEmpty, "Error should match")
+	}
+
+}
+
+func TestRegisterValidateEmail(t *testing.T) {
+
+	var err error
+
+	register := RegisterModel{
+		Name:     "test",
+		Email:    "fart@blah",
+		Password: "password",
+	}
+
+	err = register.Validate()
+	if assert.Error(t, err, "An error was expected") {
+		assert.Equal(t, err, e.ErrInvalidEmail, "Error should match")
+	}
+
+}
+
+func TestRegisterValidateEmailMissing(t *testing.T) {
+
+	var err error
+
+	register := RegisterModel{
+		Name:     "test",
+		Email:    "",
+		Password: "password",
+	}
+
+	err = register.Validate()
+	assert.NoError(t, err, "An error was not expected")
 
 }
 
@@ -69,5 +152,23 @@ func TestRegister(t *testing.T) {
 	assert.NoError(t, err, "An error was not expected")
 
 	assert.Equal(t, register.Uid, uint(2), "Error should match")
+
+}
+
+func TestRegisterInvalid(t *testing.T) {
+
+	var err error
+
+	register := RegisterModel{
+		Name:     "",
+		Password: "blah",
+		Email:    "test@blah.com",
+		Hashed:   []byte("fake"),
+	}
+
+	err = register.Register()
+	if assert.Error(t, err, "An error was expected") {
+		assert.Equal(t, err, errors.New("RegisterModel is not valid"), "Error should match")
+	}
 
 }
