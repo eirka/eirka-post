d95f3cb81ce66f5814d4095073ab488d21a07665
techjanitor <puntme@gmail.com>
2016-03-06 00:14:17 -0600

    add model tests

--- models/addtag_test.go
+++ models/addtag_test.go
@@ -1,12 +1,31 @@
 package models
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"testing"
 
 	"github.com/eirka/eirka-libs/db"
+	e "github.com/eirka/eirka-libs/errors"
 )
+
+func TestAddTagIsValid(t *testing.T) {
+
+	var err error
+
+	tag := AddTagModel{
+		Ib:    0,
+		Tag:   1,
+		Image: 1,
+	}
+
+	err = tag.Status()
+	if assert.Error(t, err, "An error was not expected") {
+		assert.Equal(t, err, errors.New("AddTagModel is not valid"), "Error should match")
+	}
+
+}
 
 func TestAddTagStatus(t *testing.T) {
 
@@ -29,6 +48,58 @@ func TestAddTagStatus(t *testing.T) {
 
 	err = tag.Status()
 	assert.NoError(t, err, "An error was not expected")
+
+}
+
+func TestAddTagStatusNotFound(t *testing.T) {
+
+	var err error
+
+	mock, err := db.NewTestDb()
+	assert.NoError(t, err, "An error was not expected")
+
+	statusrows := sqlmock.NewRows([]string{"count"}).AddRow(0)
+	mock.ExpectQuery(`SELECT count\(1\) FROM images`).WillReturnRows(statusrows)
+
+	duperows := sqlmock.NewRows([]string{"count"}).AddRow(0)
+	mock.ExpectQuery(`select count\(1\) from tagmap`).WillReturnRows(duperows)
+
+	tag := AddTagModel{
+		Ib:    1,
+		Tag:   1,
+		Image: 1,
+	}
+
+	err = tag.Status()
+	if assert.Error(t, err, "An error was not expected") {
+		assert.Equal(t, err, e.ErrNotFound, "Error should match")
+	}
+
+}
+
+func TestAddTagStatusDuplicate(t *testing.T) {
+
+	var err error
+
+	mock, err := db.NewTestDb()
+	assert.NoError(t, err, "An error was not expected")
+
+	statusrows := sqlmock.NewRows([]string{"count"}).AddRow(1)
+	mock.ExpectQuery(`SELECT count\(1\) FROM images`).WillReturnRows(statusrows)
+
+	duperows := sqlmock.NewRows([]string{"count"}).AddRow(1)
+	mock.ExpectQuery(`select count\(1\) from tagmap`).WillReturnRows(duperows)
+
+	tag := AddTagModel{
+		Ib:    1,
+		Tag:   1,
+		Image: 1,
+	}
+
+	err = tag.Status()
+	if assert.Error(t, err, "An error was not expected") {
+		assert.Equal(t, err, e.ErrDuplicateTag, "Error should match")
+	}
 
 }
 
