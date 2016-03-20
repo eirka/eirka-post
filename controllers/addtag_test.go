9f3a1d704f9ad6e9fcc1aded5a6bc5ba6ba4660a
techjanitor <puntme@gmail.com>
2016-03-20 14:50:08 -0500

    add redis mock

--- controllers/addtag_test.go
+++ controllers/addtag_test.go
@@ -71,7 +71,7 @@ func TestAddTagController(t *testing.T) {
 		WithArgs(1, 1).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	conn := redis.RedisStore.Pool.Get()
+	conn := redis.RedisCache.Pool.Get()
 	conn.Command("DEL", "tags:1", "tag:1:1", "image:1")
 
 	first := performRequest(router, "POST", "/tag/add")
