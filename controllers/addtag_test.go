266ae9010e93eb7031be72c1d11699ff137b5931
techjanitor <puntme@gmail.com>
2016-03-20 13:52:09 -0500

    add controller test

---
+++ controllers/addtag_test.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/eirka/eirka-libs/audit"
+	"github.com/eirka/eirka-libs/db"
+	"github.com/eirka/eirka-libs/user"
+)
+
+func performJsonRequest(r http.Handler, method, path string, body []byte) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func errorMessage(err error) string {
+	return fmt.Sprintf(`{"error_message":"%s"}`, err)
+}
+
+func successMessage(message string) string {
+	return fmt.Sprintf(`{"success_message":"%s"}`, message)
+}
+
+func TestAddTagController(t *testing.T) {
+
+	var err error
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+
+	router.Use(user.Auth(false))
+
+	router.POST("/tag/add", AddTagController)
+
+	mock, err := db.NewTestDb()
+	assert.NoError(t, err, "An error was not expected")
+
+	statusrows := sqlmock.NewRows([]string{"count"}).AddRow(1)
+	mock.ExpectQuery(`SELECT count\(1\) FROM images`).WillReturnRows(statusrows)
+
+	duperows := sqlmock.NewRows([]string{"count"}).AddRow(0)
+	mock.ExpectQuery(`select count\(1\) from tagmap`).WillReturnRows(duperows)
+
+	mock.ExpectExec("INSERT into tagmap").
+		WithArgs(1, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	first := performRequest(router, "POST", "/tag/add")
+
+	assert.Equal(t, first.Code, 400, "HTTP request code should match")
+
+	request1 := []byte(`{"ib": 1, "tag": 1, "image": 1}`)
+
+	second := performJsonRequest(router, "POST", "/tag/add", request1)
+
+	assert.Equal(t, second.Code, 200, "HTTP request code should match")
+	assert.JSONEq(t, second.Body.String(), successMessage(audit.AuditAddTag), "HTTP response should match")
+
+}
