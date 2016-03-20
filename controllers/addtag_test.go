97c84fcde80c98e2c0b1325542a7021d826f1877
techjanitor <puntme@gmail.com>
2016-03-20 14:31:16 -0500

    add redis mock

--- controllers/addtag_test.go
+++ controllers/addtag_test.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	//"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	//"github.com/eirka/eirka-libs/audit"
-	//"github.com/eirka/eirka-libs/db"
+	"github.com/eirka/eirka-libs/audit"
+	"github.com/eirka/eirka-libs/db"
 	"github.com/eirka/eirka-libs/redis"
 	"github.com/eirka/eirka-libs/user"
 )
@@ -48,7 +48,7 @@ func successMessage(message string) string {
 
 func TestAddTagController(t *testing.T) {
 
-	//var err error
+	var err error
 
 	gin.SetMode(gin.ReleaseMode)
 
@@ -58,28 +58,28 @@ func TestAddTagController(t *testing.T) {
 
 	router.POST("/tag/add", AddTagController)
 
-	//mock, err := db.NewTestDb()
-	//assert.NoError(t, err, "An error was not expected")
-	//
-	//statusrows := sqlmock.NewRows([]string{"count"}).AddRow(1)
-	//mock.ExpectQuery(`SELECT count\(1\) FROM images`).WillReturnRows(statusrows)
-	//
-	//duperows := sqlmock.NewRows([]string{"count"}).AddRow(0)
-	//mock.ExpectQuery(`select count\(1\) from tagmap`).WillReturnRows(duperows)
-	//
-	//mock.ExpectExec("INSERT into tagmap").
-	//	WithArgs(1, 1).
-	//	WillReturnResult(sqlmock.NewResult(1, 1))
+	mock, err := db.NewTestDb()
+	assert.NoError(t, err, "An error was not expected")
+
+	statusrows := sqlmock.NewRows([]string{"count"}).AddRow(1)
+	mock.ExpectQuery(`SELECT count\(1\) FROM images`).WillReturnRows(statusrows)
+
+	duperows := sqlmock.NewRows([]string{"count"}).AddRow(0)
+	mock.ExpectQuery(`select count\(1\) from tagmap`).WillReturnRows(duperows)
+
+	mock.ExpectExec("INSERT into tagmap").
+		WithArgs(1, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
 
 	first := performRequest(router, "POST", "/tag/add")
 
 	assert.Equal(t, first.Code, 400, "HTTP request code should match")
 
-	//request1 := []byte(`{"ib": 1, "tag": 1, "image": 1}`)
-	//
-	//second := performJsonRequest(router, "POST", "/tag/add", request1)
-	//
-	//assert.Equal(t, second.Code, 200, "HTTP request code should match")
-	//assert.JSONEq(t, second.Body.String(), successMessage(audit.AuditAddTag), "HTTP response should match")
+	request1 := []byte(`{"ib": 1, "tag": 1, "image": 1}`)
+
+	second := performJsonRequest(router, "POST", "/tag/add", request1)
+
+	assert.Equal(t, second.Code, 200, "HTTP request code should match")
+	assert.JSONEq(t, second.Body.String(), successMessage(audit.AuditAddTag), "HTTP response should match")
 
 }
