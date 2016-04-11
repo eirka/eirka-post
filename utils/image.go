fb7a7f9344ff712ff2cfcb51e92b3e381a303c74
techjanitor <puntme@gmail.com>
2016-04-10 20:55:27 -0500

    try new filename generator

--- utils/image.go
+++ utils/image.go
@@ -32,6 +32,9 @@ import (
 	local "github.com/eirka/eirka-post/config"
 )
 
+// Allowed chars for filenames
+const fileChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 // valid file extensions
 var validExt = map[string]bool{
 	".jpg":  true,
@@ -491,7 +494,7 @@ func (i *ImageType) saveFile() (err error) {
 func (i *ImageType) makeFilenames() {
 
 	// get new short id generator
-	sid := shortid.MustNew(1, shortid.DefaultABC, 9001)
+	sid := shortid.MustNew(1, fileChars, 9001)
 
 	// generate filename
 	filename := sid.MustGenerate()
